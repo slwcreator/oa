ef17cd4f7e483e5c6b8a588bf25a54ea2f79ad7a
slwer <1047579002@qq.com>
2023-08-31 10:55:03 +0800

    feat: 新增 Md5Utils 工具类

---
+++ src/main/java/com/slwer/oa/utils/Md5Utils.java
@@ -0,0 +1,14 @@
+package com.slwer.oa.utils;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class Md5Utils {
+    public static String md5Digest(String source, Integer salt) {
+        char[] chars = source.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = (char) (chars[i] + salt);
+        }
+        String target = new String(chars);
+        return DigestUtils.md5Hex(target);
+    }
+}
